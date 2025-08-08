3aaf7fa1e1390de9dc673a8476c5423b91f22595
Vikash Kumar <vikash.kumar@mathco.com>
2025-08-08 18:00:51 +0530

    feat : expandable table

--- NET - GIT SYNC TEST/screens/3__SCREEN E/widgets/0_widget/visual_uiac.py
+++ NET - GIT SYNC TEST/screens/3__SCREEN E/widgets/0_widget/visual_uiac.py
@@ -1,8 +1,17 @@
 import pandas as pd
-from codex_widget_factory_lite.visuals.simple_table import SimpleTable
+from codex_widget_factory_lite.visuals.expandable_table import ExpandableTable
 # Hardcoding a sample dataframe here, please ingest you dataset or create
-sample_df = pd.DataFrame(data = [['tom','US',10], ['nick','US',15],
-    ['juli','US',14]], columns=['Name','country','Age'])
-simpletable_output= SimpleTable(df = sample_df)
-simpletable_output.add_tooltip(isTooltip=True,tooltip_text="This is a tooltip",placement="top")
-dynamic_outputs = simpletable_output.json_string
+expandable_df = pd.DataFrame(
+    columns=['Region', 'Country', 'Random Metric'],
+    data=[
+        ['North America', 'USA', 100],
+        ['North America', 'USA', 200],
+        ['North America', 'Canada', 10],
+        ['North America', 'Canada', 20],
+    ]
+)
+value_cols_aggfunc_dict = { "Random Metric": "sum" }
+output= ExpandableTable(expandable_df = expandable_df,
+  value_cols_aggfunc_dict = value_cols_aggfunc_dict)
+output.add_tooltip(isTooltip=True,tooltip_text="This is a tooltip" ,placement="top")
+dynamic_outputs=output.json_string
